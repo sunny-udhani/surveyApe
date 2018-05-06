de76c142770311a10f5fca59ec6728fb25506804
sunny-udhani <sunny.iv09@gmail.com>
2018-05-06 01:23:20 -0700

    check attendees on publish

--- SurveyApeBackend/src/main/java/com/surveyApe/aspect/PublishAspect.java
+++ SurveyApeBackend/src/main/java/com/surveyApe/aspect/PublishAspect.java
@@ -1,18 +1,29 @@
 package com.surveyApe.aspect;
 
+import com.surveyApe.config.SurveyTypeEnum;
+import com.surveyApe.entity.Survey;
+import com.surveyApe.service.SurveyService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Aspect
 @Component
 public class PublishAspect {
+
+    @Autowired
+    private SurveyService surveyService;
 
     @Pointcut("within(@org.springframework.stereotype.Controller *) && " +
             "@annotation(requestMapping) && " +
@@ -25,18 +36,70 @@ public class PublishAspect {
     public Object beforeAll(ProceedingJoinPoint joinPoint, PostMapping requestMapping) throws Throwable {
         System.out.println(joinPoint.getSignature());
         System.out.println(joinPoint.getSignature().getName());
+
+        String methodName = joinPoint.getSignature().getName();
+
         Object[] args = joinPoint.getArgs();
         String requestBody = (String) args[0];
+        Map<String, String> requestParams = (Map<String, String>) args[1];
         JSONObject reqObj = new JSONObject(requestBody);
 
         String publishInd = (reqObj.has("publishInd")) ? reqObj.getString("publishInd") : "";
 
-        if (publishInd.equals("")){
+        if (publishInd.equals("")) {
             return joinPoint.proceed();
-        }else{
+        } else {
             int intPublishInd = Integer.parseInt(publishInd);
-            if (intPublishInd == 1){
+            if (intPublishInd == 1) {
 
+                if (methodName.contains("edit")) {
+
+                    int surveyType = Integer.parseInt(reqObj.getString("surveyType"));
+
+                    if (!surveyService.validSurveyType(surveyType)) {
+                        return new ResponseEntity<Object>("Invalid Survey Type", HttpStatus.BAD_REQUEST);
+                    }
+                    if (surveyType == SurveyTypeEnum.CLOSED.getEnumCode()) {
+
+                        if (!(reqObj.has("attendeesList"))) {
+                            String surveyId = (String) args[2];
+
+                            Survey survey = surveyService.findBySurveyId(surveyId);
+                            if (survey == null) {
+                                return new ResponseEntity<Object>("No such survey", HttpStatus.BAD_REQUEST);
+                            }
+
+                            if (survey.getResponseList() != null) {
+                                int numberOfattendees = survey.getResponseList().size();
+                                if (numberOfattendees > 0)
+                                    return joinPoint.proceed();
+                                else
+                                    return new ResponseEntity<Object>("No attendees added, so can't publish", HttpStatus.BAD_REQUEST);
+                            } else
+                                return new ResponseEntity<Object>("No attendees added, so can't publish", HttpStatus.BAD_REQUEST);
+                        } else
+                            return joinPoint.proceed();
+//                        JSONArray attendeesArray = reqObj.getJSONArray("attendeesList");
+                    }
+
+                } else {
+                    int surveyType = Integer.parseInt(reqObj.getString("surveyType"));
+
+                    if (!surveyService.validSurveyType(surveyType)) {
+                        return new ResponseEntity<Object>("Invalid Survey Type", HttpStatus.BAD_REQUEST);
+                    }
+                    if (surveyType == SurveyTypeEnum.CLOSED.getEnumCode()) {
+
+                        if (!(reqObj.has("attendeesList")))
+                            return new ResponseEntity<Object>("No attendees added, so can't publish", HttpStatus.BAD_REQUEST);
+                        else
+                            return joinPoint.proceed();
+//                        JSONArray attendeesArray = reqObj.getJSONArray("attendeesList");
+                    }
+                }
+
+            } else {
+                return joinPoint.proceed();
             }
         }
 //
