5212d4ae23c2bc4b25d1a6100fc1fc02bd6c689d
sunny-udhani <sunny.iv09@gmail.com>
2018-05-20 01:42:57 -0700

    new email+submit survey :octocat:

---
+++ SurveyApeBackend/src/main/java/com/surveyApe/config/Config.java
@@ -0,0 +1,9 @@
+package com.surveyApe.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    public static final String EMAIL_NOREPLY = "surveyape.noreply@gmail.com";
+}
